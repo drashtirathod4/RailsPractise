f153c5cae7b10a002eed4413e157e6432cab6eff
drashtirathod21 <drashtir9@gmail.com>
2022-03-22 17:34:54 +0530

    Implemented active storage overview 22 march

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    delete :delete_attachement, on: :member
+    delete :delete_post_attachement, on: :member
+  end
+  # delete '/users/:id/delete_attachement/:post_id', controller: :delete_post_attachement, action: :delete
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
