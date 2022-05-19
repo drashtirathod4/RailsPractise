12f2902f43dd4bc9697d057bdeffd4bf97a16ddd
drashtirathod4 <drashti.r@simformsolutions.com>
2022-05-19 11:19:38 +0530

    feat: Implemented jwt authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :jwt_users,
+    controllers: {
+      sessions: 'jwt_users/sessions',
+      registrations: 'jwt_users/registrations'
+  }
+  get '/member-data', to: 'members#show'
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_041728) do
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_100221) do
   create_table "ads", force: :cascade do |t|
     t.string "ad_name"
     t.integer "magazine_id", null: false
@@ -55,10 +27,37 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_041728) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
+  create_table "graphql_users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "images", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
+  end
+
+  create_table "jwt_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_jwt_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_jwt_users_on_reset_password_token", unique: true
   end
 
   create_table "magazines", force: :cascade do |t|
@@ -67,35 +66,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_041728) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "my_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
+  create_table "posts", force: :cascade do |t|
+    t.integer "graphql_user_id", null: false
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "phone"
-    t.text "address"
-    t.string "role"
-    t.index ["email"], name: "index_my_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_my_users_on_reset_password_token", unique: true
+    t.index ["graphql_user_id"], name: "index_posts_on_graphql_user_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.integer "user_id", null: false
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
   end
 
   create_table "users", force: :cascade do |t|
@@ -106,16 +94,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_041728) do
     t.boolean "agreement"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
     t.string "course"
     t.date "start_date"
     t.date "end_date"
-    t.string "document"
+    t.string "password_digest"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  end
+
   add_foreign_key "ads", "magazines"
   add_foreign_key "articles", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key "posts", "graphql_users"
 end
