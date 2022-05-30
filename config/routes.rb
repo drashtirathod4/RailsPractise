3859ae9b3dc90529a5d5383b1754dcff2ed30106
drashtirathod4 <drashti.r@simformsolutions.com>
2022-05-30 14:54:21 +0530

    feat: add scaffold messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+    root "messages#index"
+  resources :messages
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
